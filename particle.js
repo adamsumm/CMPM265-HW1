95265652cebe8374d2ce69ac104786cb5e1620eb
adamsumm <adamsumm@gmail.com>
2016-01-06 17:19:21 +0000

    Everything Working

--- particle.js
+++ particle.js
@@ -10,22 +10,31 @@ class Particle{
         this.life = 5;
         this.lifetime = this.life;
         this.game = game;
+        this.sprite = game.add.sprite(xx, yy, 'apple');
     }
     update(dt){
         this.vx += this.ax*dt;
         this.vy += this.ay*dt;
         this.xx += this.vx*dt;
         this.yy += this.vy*dt;
+        this.sprite.x = this.xx;
+        this.sprite.y = this.yy;
         this.lifetime -= dt;
     }
     render(){
-        this.game.context.fillStyle = 'rgb(255,255,255)';
-        this.game.context.fillRect(this.xx, this.yy, 1, 1);
+      //  this.game.context.fillStyle = 'rgb(255,255,255)';
+    //    this.game.context.fillRect(this.xx, this.yy, 1, 1);
     }
 }
-
+class Attractor{
+    constructor(xx,yy,strength){
+        this.xx = xx;
+        this.yy = yy;
+        this.strength = strength;
+    }
+}
 class ParticleEmitter{
-    constructor(xx,yy,rate,anglemin,anglemax,minvel,maxvel,initialAmount,game){
+    constructor(xx,yy,rate,anglemin,anglemax,minvel,maxvel,minlife,maxlife,initialAmount,game){
         this.xx = xx;
         this.yy = yy;
         this.rate = rate;
@@ -38,7 +47,8 @@ class ParticleEmitter{
         this.game = game;
         this.maxvel = maxvel;
         this.minvel = minvel;
-        
+        this.minlife = minlife;
+        this.maxlife = maxlife;
         for (var ii = 0; ii < initialAmount; ii++){
             this.pooledParticles.push(new Particle(0,0,0,0,game));
         }
@@ -49,25 +59,30 @@ class ParticleEmitter{
         while(this.timeSinceLastParticle > 0){
             this.timeSinceLastParticle -= this.period;
             if (this.pooledParticles.length > 0){
-                var particle = this.pooledParticles.pop();
+                var particle = this.pooledParticles.shift();
             }
-            else {
+            else { 
                 var particle = new Particle(0,0,0,0,this.game);
             }
             particle.xx = this.xx;
             particle.yy = this.yy;
+            var life = Math.random() * (this.maxlife - this.minlife) + this.minlife;
+            
             var angle = Math.random() * (this.anglemax - this.anglemin) + this.anglemin;
             var speed = Math.random() * (this.maxvel - this.minvel) + this.minvel;
             particle.vx = Math.cos(angle)*speed;
             particle.vy = Math.sin(angle)*speed;
-            particle.lifetime = particle.life;
+            particle.lifetime = life;
+            particle.sprite.visible = true;
             this.activeParticles.push(particle);
         }
         for (var ii = 0; ii < this.activeParticles.length; ii++){
             this.activeParticles[ii].update(dt);
             if (this.activeParticles[ii].lifetime <= 0){
                 this.pooledParticles.push(this.activeParticles[ii]);
+                this.activeParticles[ii].visible = false;
                 this.activeParticles.splice(ii, 1);
+                
             }
         }
     }
